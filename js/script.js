957ee3f1364ca4e6217438cb1f8ea7af296a3892
WalkingFrozenFish <chachin.ol@yandex.kz>
2021-04-03 01:18:54 +0600

    Добавил возможность копирования цвета в буфер обмена по клику

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
+// Кнопка для генерации цвета
 let button = document.querySelector(".button");
 button.addEventListener("click", generateColor);
 
+// Копирование кода в буфер обмена
+let copy = document.querySelector(".hexy");
+copy.addEventListener("click", function(event) {
+    let elem = event.target;
+    navigator.clipboard.writeText(elem.innerHTML);
+});
+
 function generateColor() {
+    // Массивы для генерации
     let arrChars = ["A", "B", "C", "D", "E", "F"];
     let arrNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+    // Массив результат
     let resultColor = [];
 
     for (var i = 0; i < 6; i++) {
+        // По рандомному числу определяем что будет в коде, буква или цифра
         let randomNum = Math.floor(Math.random() * 2) + 1;
         // let randomNum = Math.floor(Math.random() * 2);
 
+        // Если единица, то в результат пойдет число, иначе буква
         if (randomNum == 1) {
             resultColor.push(Math.floor(Math.random() * (arrNums.length - 0)) + 0);
             // resultColor.push(Math.floor(Math.random() * arrNums.length));
@@ -21,6 +33,7 @@ function generateColor() {
 
     }
 
+    // Генерируем код цвета
     console.log(`Result color: #${resultColor.join("")}`);
     document.querySelector(".colors").style.backgroundColor = "#" + resultColor.join("");
     document.querySelector(".hexy").textContent = "#" + resultColor.join("")
